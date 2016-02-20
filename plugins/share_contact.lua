409c7d6cbc8c66a7f7f0d8ff3605398c02848143
77shahriar77 <shahriar.jalili8888@yahoo.com>
2016-02-20 13:38:39 -0800

    Create share_contact.lua

---
+++ plugins/share_contact.lua
@@ -0,0 +1,17 @@
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "YOUR BOT PHONE", "FRIST NAME", "LAST NAME", ok_cb, false)
+end
+
+return {
+patterns = {
+"^[!/]share$"
+
+},
+run = run
+}
+
+end
